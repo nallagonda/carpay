45af7606e411a0927b2e3f808ad74b6671129592
KiranKumar Nallagonda <nallagondakk@gmail.com>
2023-09-10 08:37:59 -0700

    contract code

--- target2/release/build/schemars-af53339fbe5ffa87/build_script_build-af53339fbe5ffa87.d
+++
@@ -1,5 +0,0 @@
-/Users/nallagonda/Kiran/seilab/contract/carpay/target/release/build/schemars-af53339fbe5ffa87/build_script_build-af53339fbe5ffa87: /Users/nallagonda/.cargo/registry/src/index.crates.io-6f17d22bba15001f/schemars-0.8.13/build.rs
-
-/Users/nallagonda/Kiran/seilab/contract/carpay/target/release/build/schemars-af53339fbe5ffa87/build_script_build-af53339fbe5ffa87.d: /Users/nallagonda/.cargo/registry/src/index.crates.io-6f17d22bba15001f/schemars-0.8.13/build.rs
-
-/Users/nallagonda/.cargo/registry/src/index.crates.io-6f17d22bba15001f/schemars-0.8.13/build.rs:
